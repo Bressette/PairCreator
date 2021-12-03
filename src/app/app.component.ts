909fdcc27c40dfbdabe2896219f431f020bf7a0b
Johnathan Bressette <bressetteprogramming@gmail.com>
2021-12-02 19:58:18 -0500

    Add basic layout for the pair creator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private formBuilder: FormBuilder) {}
+
   title = 'PairCreator';
+  groupSize = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  formGroup = this.formBuilder.group({
+    groupNames: ['Test, Test, Test, Test, Test, Test, Test, Test'],
+    groupSize: [4],
+    incompatibleNames: [''],
+    previousPairs: [''],
+  });
+
 }
