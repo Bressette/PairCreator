9c8a830e76fbf1ac938f041d93151901117bf206
Johnathan Bressette <bressetteprogramming@gmail.com>
2021-12-02 20:56:54 -0500

    Add generatePairs function to generate random groups of pairs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FormBuilder} from '@angular/forms';
+import {PairGroup} from './pair';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +9,43 @@ import {FormBuilder} from '@angular/forms';
 })
 export class AppComponent {
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder) {
+    this.generatePairs();
+  }
 
   title = 'PairCreator';
-  groupSize = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  pairs: PairGroup[] = [];
+  pairString: string[] = [];
 
   formGroup = this.formBuilder.group({
-    groupNames: ['Test, Test, Test, Test, Test, Test, Test, Test'],
-    groupSize: [4],
+    groupNames: ['Bryce, Ian, Matthew, Johnathan, Amy, Billy, Walter, Taylor'],
+    groupSize: [2],
     incompatibleNames: [''],
     previousPairs: [''],
   });
+
+  generatePairs(): void {
+    this.pairs = [];
+    this.pairString = [];
+    let names: string[];
+    names = this.formGroup.get('groupNames')?.value.split(',').map((result: string) => result.trim());
+    while (names.length > 0) {
+      for (let j = 0; j < this.formGroup.get('groupSize')?.value; j++) {
+        if (this.pairs.length < j + 1) {
+          this.pairs.push(new PairGroup());
+        }
+        const selectedIndex = Math.floor(Math.random() * names.length);
+        this.pairs[j].names.push(names[selectedIndex]);
+        names.splice(selectedIndex, 1);
+      }
+    }
+
+    for (const pair of this.pairs) {
+      console.log(pair.names.join(','));
+      this.pairString.push(pair.names.join(', '));
+    }
+    console.log('The pairs object after generating the pairs: ' + JSON.stringify(this.pairs));
+    console.log('The pairString object after parsing the string: ' + JSON.stringify(this.pairString));
+  }
 
 }
